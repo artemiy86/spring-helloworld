2b8e5426618546a04e11171f5efd1b319ee8ad7e
artemiy86 <artemiy86@bk.ru>
2021-04-27 11:37:40 +0300

    2.1.2 complete

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
